4aa8afd3a87b6aaa1384156fa44becbd558ca36b
randomguy <random@guy>
2022-01-23 21:40:49 +0100

    added variable ImplQoQRevBooking which is revenue + delta deferral growth

--- bundle_main/datasets_merge.py
+++ bundle_main/datasets_merge.py
@@ -20,11 +20,10 @@ financials_q = pd.read_csv(os.path.join(cwd,input_folder,"3_processed_financials
 other_table = pd.read_csv(os.path.join(cwd,input_folder,"3_processed_other.csv"), low_memory=False)
 
 # select only latest year and last X years
-financials_a_last = financials_a.sort_values(['symbol','date'], ascending=False).groupby('symbol').head(1)
+financials_a_latest = financials_a.sort_values(['symbol','date'], ascending=False).groupby('symbol').head(1)
 financials_a_X = financials_a.sort_values(['symbol','date'], ascending=False).groupby('symbol').head(5)
 
 # historical calc
-financials_a_last.rename(columns={'revenue': 'revenue_last_y'}, inplace=True)
 financials_mean = financials_a_X.groupby(['symbol'])[['revenue'
                         , 'capitalExpenditure', 'costOfRevenue'
                         , 'propertyPlantEquipmentNet']].mean()
@@ -33,22 +32,29 @@ financials_mean.rename(columns={'revenue': 'mean_revenue'
                        , 'costOfRevenue': 'mean_costOfRevenue'
                        , 'propertyPlantEquipmentNet': 'mean_PPEnet'}
                        , inplace=True)
-print("historical averages calculated")
 
-# merge last annual financials, and prices
-df_merged = pd.merge(financials_a_last, prices_table
+# merge annual latest and mean
+df_merged = pd.merge(financials_a_latest, financials_mean
+                     , how='left', left_on=['symbol']
+                     , right_on=['symbol'], suffixes=('', '_drop'))
+df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
+df_annual_current_mean = df_merged
+
+# merge prices
+df_merged = pd.merge(df_annual_current_mean, prices_table
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
 df_merged.rename(columns={'yearHigh': '52h'
     , 'yearLow': '52l'
     , 'price': 'p'}, inplace=True)
-df_last_ann_and_prices = df_merged
+df_annual_current_mean_prices = df_merged
 
-# calculate YoY
-financials_a_minus_one_last = financials_a.sort_values(['symbol','date']
+# calculate annual minus one
+financials_a_minus_one = financials_a.sort_values(['symbol','date']
                         , ascending=False).groupby('symbol').head(2).drop_duplicates(['symbol'], keep='last')
-financials_a_minus_one_last.rename(columns={'revenue': 'revenue_minus_one_y'
+financials_a_minus_one.rename(columns={'revenue': 'revenue_minus_one_y'
+                        , 'deferredRevenue':'deferredRevenue_minus_one_y'
                         , 'operatingExpenses':'operatingExpenses_minus_one_y'
                         , 'totalStockholdersEquity' : 'totalStockholdersEquity_minus_one_y'
                         , 'totalCurrentAssets':'totalCurrentAssets_minus_one_y'
@@ -57,23 +63,37 @@ financials_a_minus_one_last.rename(columns={'revenue': 'revenue_minus_one_y'
                         , 'netCashProvidedByOperatingActivites': 'ncfo_minus_one_y'
                                   }
                         , inplace=True)
-# YoY merge
-df_merged = pd.merge(df_last_ann_and_prices, financials_a_minus_one_last
+# merge annual minus one
+df_merged = pd.merge(df_annual_current_mean_prices, financials_a_minus_one
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-df_last_ann_and_prices_and_YoY = df_merged
+df_annual_current_mean_prices_minus_one = df_merged
 
-# merge historical financials
-df_merged = pd.merge(df_last_ann_and_prices_and_YoY, financials_mean
+# calculate 2 years ago YoY
+financials_a_minus_two = financials_a.sort_values(['symbol','date']
+                        , ascending=False).groupby('symbol').head(3).drop_duplicates(['symbol'], keep='last')
+financials_a_minus_two.rename(columns={'revenue': 'revenue_minus_two_y'
+                        , 'deferredRevenue':'deferredRevenue_minus_two_y'
+                        , 'operatingExpenses':'operatingExpenses_minus_two_y'
+                        , 'totalStockholdersEquity' : 'totalStockholdersEquity_minus_two_y'
+                        , 'totalCurrentAssets':'totalCurrentAssets_minus_two_y'
+                        , 'totalCurrentLiabilities':'totalCurrentLiabilities_minus_two_y'
+                        , 'netDebt':'netDebt_minus_one_y'
+                        , 'netCashProvidedByOperatingActivites': 'ncfo_minus_one_y'
+                                  }
+                        , inplace=True)
+# merge annual minus two
+df_merged = pd.merge(df_annual_current_mean_prices_minus_one, financials_a_minus_two
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-df_last_ann_and_prices_and_YoY_and_mean = df_merged
+df_annual_current_mean_prices_minus_one_two = df_merged
 
 # prepare last quarter
 financials_q_last = financials_q.sort_values(['symbol','date'], ascending=False).groupby('symbol').head(1)
 financials_q_last.rename(columns={'revenue': 'revenue_last_q'
+                        , 'deferredRevenue':'deferredRevenue_last_q'
                         , 'operatingExpenses':'operatingExpenses_last_q'
                         , 'totalStockholdersEquity' : 'totalStockholdersEquity_last_q'
                         , 'totalCurrentAssets':'totalCurrentAssets_last_q'
@@ -84,26 +104,51 @@ financials_q_last.rename(columns={'revenue': 'revenue_last_q'
                         , inplace=True)
 
 # prepare last minus one quarter
-financials_q_minus_one_last = financials_q.sort_values(['symbol','date']
+financials_q_minus_one = financials_q.sort_values(['symbol','date']
                         , ascending=False).groupby('symbol').head(2).drop_duplicates(['symbol'], keep='last')
-financials_q_minus_one_last.rename(columns={'revenue': 'revenue_minus_one_q'
+financials_q_minus_one.rename(columns={'revenue': 'revenue_minus_one_q'
+                        , 'deferredRevenue':'deferredRevenue_minus_one_q'
                         , 'operatingExpenses':'operatingExpenses_minus_one_q'
+                        , 'totalStockholdersEquity' : 'totalStockholdersEquity_minus_one_q'
+                        , 'totalCurrentAssets':'totalCurrentAssets_minus_one_q'
+                        , 'totalCurrentLiabilities':'totalCurrentLiabilities_minus_one_q'
+                        , 'netDebt':'netDebt_minus_one_q'
                         , 'netCashProvidedByOperatingActivites': 'ncfo_minus_one_q'}
                         , inplace=True)
 
-# calc QoQ growth
-df_merged = pd.merge(financials_q_last, financials_q_minus_one_last
+# merg current quarter to previous quarter
+df_merged = pd.merge(financials_q_last, financials_q_minus_one
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-QoQ = df_merged
+df_q_last_and_minus_one = df_merged
 
-# merge all with QoQ
-df_merged = pd.merge(df_last_ann_and_prices_and_YoY_and_mean, QoQ
+# prepare last minus two quarter
+financials_q_minus_two = financials_q.sort_values(['symbol','date']
+                        , ascending=False).groupby('symbol').head(3).drop_duplicates(['symbol'], keep='last')
+financials_q_minus_two.rename(columns={'revenue': 'revenue_minus_two_q'
+                        , 'deferredRevenue':'deferredRevenue_minus_two_q'
+                        , 'operatingExpenses':'operatingExpenses_minus_two_q'
+                        , 'totalStockholdersEquity' : 'totalStockholdersEquity_minus_two_q'
+                        , 'totalCurrentAssets':'totalCurrentAssets_minus_two_q'
+                        , 'totalCurrentLiabilities':'totalCurrentLiabilities_minus_two_q'
+                        , 'netDebt':'netDebt_minus_two_q'
+                        , 'netCashProvidedByOperatingActivites': 'ncfo_minus_two_q'}
+                        , inplace=True)
+
+# merge current, previous, and minus two quarters
+df_merged = pd.merge(df_q_last_and_minus_one, financials_q_minus_two
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-df_last_ann_and_prices_and_YoY_and_mean_and_QQ = df_merged
+df_quarters = df_merged
+
+# merge annual, mean and quarters
+df_merged = pd.merge(df_annual_current_mean_prices_minus_one_two, df_quarters
+                     , how='left', left_on=['symbol']
+                     , right_on=['symbol'], suffixes=('', '_drop'))
+df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
+df_annual_quarters_mean_prices = df_merged
 
 # calculate TTM
 financials_TTM = financials_q.sort_values(['symbol','date'], ascending=False).groupby('symbol').head(4)
@@ -114,45 +159,58 @@ financials_TTM_sum.rename(columns={'revenue': 'revenue_TTM'
                         , inplace=True)
 
 # merge all with TTM
-df_merged = pd.merge(df_last_ann_and_prices_and_YoY_and_mean_and_QQ, financials_TTM_sum
+df_merged = pd.merge(df_annual_quarters_mean_prices, financials_TTM_sum
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-df_last_ann_and_prices_and_YoY_and_mean_and_QQ_and_TTM = df_merged
+df_annual_quarters_mean_prices_TTM = df_merged
 
 # merge all with other
-df_merged = pd.merge(df_last_ann_and_prices_and_YoY_and_mean_and_QQ_and_TTM, other_table
+df_merged = pd.merge(df_annual_quarters_mean_prices_TTM, other_table
                      , how='left', left_on=['symbol']
                      , right_on=['symbol'], suffixes=('', '_drop'))
 df_merged.drop([col for col in df_merged.columns if 'drop' in col], axis=1, inplace=True)
-df_last_ann_and_prices_and_YoY_and_mean_and_QQ_and_TTM_and_other = df_merged
+df_annual_quarters_mean_prices_TTM_other = df_merged
 
 #
 #
 #
-df = df_last_ann_and_prices_and_YoY_and_mean_and_QQ_and_TTM_and_other
+df = df_annual_quarters_mean_prices_TTM_other
 #
 #
 #
 
 # adding variables
+
+# deferred revenues
+df['booking_y'] = df['deferredRevenue'] - df['deferredRevenue_minus_one_y']
+df['booking_minus_one_y'] = df['deferredRevenue_minus_one_y'] - df['deferredRevenue_minus_two_y']
+df['booking_q'] = df['deferredRevenue_last_q'] - df['deferredRevenue_minus_one_q']
+df['booking_minus_one_q'] = df['deferredRevenue_minus_one_q'] - df['deferredRevenue_minus_two_q']
+df['rev_plus_booking_y'] = df['revenue'] + df['booking_y']
+df['rev_plus_booking_minus_one_y'] = df['revenue_minus_one_y'] + df['booking_minus_one_y']
+df['rev_plus_booking_q'] = df['revenue_last_q'] + df['booking_q']
+df['rev_plus_booking_minus_one_q'] = df['revenue_minus_one_q'] + df['booking_minus_one_q']
+
 # growth
-df['ImplYoYRev'] = df['revenue_last_y'] / df['revenue_minus_one_y'] - 1
-df['ImplYoYncfo'] = df['netCashProvidedByOperatingActivites'] / df['ncfo_minus_one_y'] - 1
-df['ImplQoQRev'] = df['revenue_last_q'] / df['revenue_minus_one_q'] -1
-df['ImplQoQncfo'] = df['netCashProvidedByOperatingActivites_last_q'] / df['ncfo_minus_one_q'] - 1
+df['ImplYoYRev'] = (df['revenue'] - df['revenue_minus_one_y']) / df['revenue_minus_one_y'] * 100
+df['ImplYoYncfo'] = (df['netCashProvidedByOperatingActivites'] - df['ncfo_minus_one_y']) / df['ncfo_minus_one_y'] * 100
+df['ImplYoYRevBooking'] = (df['rev_plus_booking_y'] - df['rev_plus_booking_minus_one_y']) / df['rev_plus_booking_minus_one_y'] * 100
+df['ImplQoQRev'] = (df['revenue_last_q'] - df['revenue_minus_one_q']) / df['revenue_minus_one_q'] * 100
+df['ImplQoQncfo'] = (df['netCashProvidedByOperatingActivites_last_q'] - df['ncfo_minus_one_q']) / df['ncfo_minus_one_q'] * 100
+df['ImplQoQRevBooking'] = (df['rev_plus_booking_q'] - df['rev_plus_booking_minus_one_q']) / df['rev_plus_booking_minus_one_q'] * 100
 
 # price
 df['from_low'] = (df_merged['p'] - df_merged['52l']) / df_merged['52l'] * 100
 df.loc[(df['from_low'] < 0), 'from_low'] = 0
 df['from_high'] = (df_merged['p'] - df_merged['52h']) / df_merged['52h'] * 100
 
-# sales
+# margins
 df['mean_OpMarg'] = (df['mean_revenue'] - df['mean_costOfRevenue']) / df['mean_revenue'] * 100
 df['marg_TTM'] = (df['revenue_TTM'] - df['operatingExpenses_last_q']) / df['revenue_TTM'] * 100
-df['Sales_absolute_increase'] = df['revenue_TTM'] - df['revenue_last_y']
 
 # capex
+df['Sales_absolute_increase'] = df['revenue_TTM'] - df['revenue']
 df['maint_capex_ratio'] = df['mean_PPEnet'] / df['mean_revenue']
 df['growth_capex'] = df['maint_capex_ratio'] * df['Sales_absolute_increase']
 df['capex_more_correct'] = df['capex_TTM'] - df['growth_capex']
@@ -177,6 +235,7 @@ print('variables calculated')
 
 # fixing
 df['ImplQoQRev'] = df['ImplQoQRev'].fillna(df['ImplYoYRev'] / 4)
+df['ImplQoQRevBooking'] = df['ImplQoQRevBooking'].fillna(df['ImplYoYRevBooking'] / 4)
 df['ImplQoQncfo'] = df['ImplQoQncfo'].fillna(df['ImplYoYncfo'] / 4)
 
 cols_to_fillna = [i for i in df.columns]
@@ -195,14 +254,15 @@ for col in cols_to_fillna:
     except:
         pass
 
-
 # format
 cols_to_format = [i for i in df.columns]
 for col in cols_to_format:
     try:
-        if col in ['p', 'B/S/p', '52l', '52h'
+        if col in [
+            'p', 'B/S/p', '52l', '52h'
             , 'Eq/D'
-            , 'ImplYoYRev', 'ImplQoQRev', 'ImplYoYncfo', 'ImplQoQncfo']:
+            , 'ImplYoYRev', 'ImplQoQRev', 'ImplYoYncfo', 'ImplQoQncfo'
+            , 'ImplQoQRevBooking', 'ImplYoYRevBooking']:
             df[col]=df[col].round(2)
         else:
             df[col] = df[col].round(0)
