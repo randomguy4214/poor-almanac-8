f1af531965e80dd3f1d0dd0bb1a430be0f752322
randomguy <random@guy>
2022-01-24 14:20:00 +0100

    added variable ImplQoQRevBooking which is revenue + delta deferral growth

--- bundle_main/update_prices_only_filtered.py
+++ bundle_main/update_prices_only_filtered.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
-
-print('prices_update_filtered - initiating.')
+print('update_prices_filtered - initiating.')
 
 import os
 import pandas as pd
-from datetime import date
-
-pd.set_option('display.max_columns', None)
-pd.options.display.float_format = '{:20,.2f}'.format
-pd.options.mode.use_inf_as_na = True
-
+import datetime
 cwd = os.getcwd()
 input_folder = "0_input"
 prices_folder = "data"
 output_folder = "0_output"
 temp_folder = "temp"
 prices_temp = "prices"
-financials_temp = "financials"
 
-#check year
-todays_date = date.today()
-curr_year = todays_date.year
+#setting up fmpcloud
+#https://fmpcloud.io/api/v3/quote/AAPL?datatype=csv&timeseries=255&apikey=
+token_df = pd.read_csv(os.path.join(cwd,"0_api_token.csv"))
+token = token_df.iloc[0,1]
+url1 = "https://fmpcloud.io/api/v3/quote/"
+apikey = "apikey="
+amp = "&"
+csv = "?datatype=csv"
 
 # prepare tickers list
 tickers_narrowed = pd.read_csv(os.path.join(cwd,input_folder,"5_tickers_filtered.csv"))
@@ -28,45 +26,32 @@ ticker_narrowed = tickers_narrowed.values.tolist()
 tickers = ' '.join(tickers_narrowed["symbol"].astype(str)).strip()
 
 # find last updated ticker (this is necessary if you lose internet connection, etc)
-prices_last_ticker = pd.read_csv(os.path.join(cwd,input_folder,temp_folder,"prices_last_ticker_filtered.csv"),index_col=0)
-last_ticker_n = prices_last_ticker.values[0]
-print("last ticker in prices update filtered was number ", last_ticker_n)
+last_ticker = pd.read_csv(os.path.join(cwd,input_folder,temp_folder,"prices_last_ticker_filtered.csv"),index_col=0)
+last_ticker_n = last_ticker.values[0]
+last_ticker_nn = last_ticker_n[0]
+print("last ticker in prices_filtered was number", last_ticker_nn)
 
-# start importing the prices
+# start importing
 index_max = pd.to_numeric(tickers_narrowed.index.values.max())
-from yahoo_fin.stock_info import * #initiate yahoo_fin
-company_info = []
-
 for t in tickers.split(' '):
     try:
         n = pd.to_numeric(tickers_narrowed["symbol"][tickers_narrowed["symbol"] == t].index).values
-        if n > last_ticker_n:
-            # check if last quarter is recent (many tickers are dead for example)
-            df_yf_stats = get_stats(t)
-            df_check_mrq = df_yf_stats["Value"][df_yf_stats["Attribute"] == "Most Recent Quarter (mrq)"]
-            datetime_object = pd.to_datetime(df_check_mrq)  # , errors='coerce')
-            df_mrq_year = datetime_object.dt.year
-            mrq_year = df_mrq_year.values[0]
-
-            if (mrq_year + 1) >= curr_year:
-                name = t + ".csv"
-                # get quote
-                df_yf_get_quote_table = get_quote_table(t, dict_result=True)
-                df = pd.DataFrame.from_dict(df_yf_get_quote_table, orient='index')
-                df = df.T
-                df['symbol'] = t
-                # export
-                df.to_csv(os.path.join(cwd, input_folder, temp_folder, prices_temp, name), index=False)
-
-                # print & export last_n
-                print(t, n/index_max*100, "% /", n, "from", index_max, " /prices filtered")
-                prices_last_ticker = pd.DataFrame({'number':n})
-                prices_last_ticker.to_csv(os.path.join(cwd, input_folder, temp_folder, "prices_last_ticker_filtered.csv"))
-
+        if n > last_ticker_nn:
+            final_url = url1 + t + csv + amp + apikey + token
+            df = pd.read_csv(final_url)
+            df['symbol'] = t
+            name = t + ".csv"
+            df.to_csv(os.path.join(cwd, input_folder, temp_folder, prices_temp, name))
+            # print & export last_n
+            nn = n[0] # get number out of numpy.array
+            nnn = round(nn/index_max*100,1)
+            print("prices:", t, "/" ,nn, "from", index_max, "/", nnn, "%")
+            last_ticker = pd.DataFrame({'number':n})
+            last_ticker.to_csv(os.path.join(cwd, input_folder, temp_folder, "prices_last_ticker_filtered.csv"))
     except:
         pass
 
 prices_last_ticker = pd.DataFrame({'number': [0] })
 prices_last_ticker.to_csv(os.path.join(cwd,input_folder,temp_folder,"prices_last_ticker_filtered.csv"))
 
-print('prices_update - done')
+print('update_prices_filtered - done')
