1cbf9b61d3db179eb850d46dfffe911a5cd7412f
randomguy <random@guy>
2022-03-08 14:48:06 +0100

    -

--- main_recent_fundamentals_only.py
+++ main_recent_fundamentals_only.py
@@ -7,6 +7,8 @@ bundle_other = "bundle_other"
 from bundle_other import put_reduced_symbols
 from bundle_update import update_financials_q
 from bundle_update import update_financials_a
+from bundle_update import update_other
 from bundle_process import process_financials_q
 from bundle_process import process_financials_a
+from bundle_process import process_other
 from bundle_other import put_original_symbols
