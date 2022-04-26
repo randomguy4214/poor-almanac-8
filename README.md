4bf4bd3120c77a34b35ce040493ff248e81e10c5
randomguy <random@guy>
2022-04-26 10:48:02 +0200

    -

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ financialmodelingprep.com subscription is required.
 
 The idea is to find ratio of current price, and discounted Owners Earnings. 
 The code is doing exactly that. 
+
 The output is a scatterplot of price change from lowest point in 52 weeks, to target price. 
 
--- main_update_prices_and_latest_fundamentals.py
+++ main_update_prices_and_latest_fundamentals.py
@@ -12,11 +12,13 @@ from bundle_other import put_reduced_symbols
 from bundle_update import update_financials_q
 from bundle_update import update_financials_a
 from bundle_update import update_other
+
 from bundle_process import process_financials_q
 from bundle_process import process_financials_a
 from bundle_process import process_other
 from bundle_process import process_EV
 from bundle_other import put_original_symbols
+
 from bundle_merge_output import a_OwnEa_annually
 from bundle_merge_output import a_OwnEa_quarterly
 from bundle_merge_output import a_OwnEa_last_8_quarters
