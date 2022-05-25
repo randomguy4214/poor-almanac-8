15a912e79e0e62bec00b965f881c27d977ac8d4a
randomguy <random@guy>
2022-05-25 09:59:07 +0200

    adding commodities

--- bundle_charts/plot_commodities.py
+++ bundle_charts/plot_commodities.py
@@ -28,20 +28,20 @@ crude_oil = commodities_df.iloc[:,[0,1,2,3]]
 coal = commodities_df.iloc[:,[4,5]]
 gas = commodities_df.iloc[:,[6,7,8]]
 gas_index = commodities_df.iloc[:,[9]]
-cocoa_coffee = commodities_df.iloc[:,[10,11,12]]
+cocoa_coffee = commodities_df.iloc[:,[10,11,12, 47]]
 tea = commodities_df.iloc[:,[13,14,15,16]]
-oils = commodities_df.iloc[:,[17,21,22]]
+oils = commodities_df.iloc[:,[17,21,22,24]]
 beans = commodities_df.iloc[:,[18,23,25,27]]
-plants = commodities_df.iloc[:,[19,20,24,26,28,29,30,31,32,33,34,35]]
-rice = commodities_df.iloc[:,[32,33,34,35]]
-wheat = commodities_df.iloc[:,[36,37]]
-fruits = commodities_df.iloc[:,[38,39,40]]
-meat = commodities_df.iloc[:,[41,42,43,44]]
-sugar = commodities_df.iloc[:,[45,46]]
-wood = commodities_df.iloc[:,[49,50,51,52,53]]
-materials_one = commodities_df.iloc[:,[48,54,55,56]]
-fertilizers = commodities_df.iloc[:,[58,59,60,61]]
-#metals = commodities_df.iloc[:,[62,63,64,65,66,67,68,69,70,71]]
+plants = commodities_df.iloc[:,[19,20,26,28,29,30]]
+rice = commodities_df.iloc[:,[31,32,33,34]]
+wheat = commodities_df.iloc[:,[35,36]]
+fruits = commodities_df.iloc[:,[37,38,39]]
+meat = commodities_df.iloc[:,[40,41,42,43]]
+sugar = commodities_df.iloc[:,[44,45,46]]
+wood = commodities_df.iloc[:,[48,49,50,51,52]]
+materials_one = commodities_df.iloc[:,[53,54,55]]
+fertilizers = commodities_df.iloc[:,[56,57,58,59,60]]
+metals = commodities_df.iloc[:,[61,62,63,64,65,66,67,68,69,70]]
 
 # create a list of dataframes and loop through them to create separate charts
 alldfs = [var for var in dir() if isinstance(eval(var), pd.core.frame.DataFrame)]
@@ -62,7 +62,7 @@ for df_name in alldfs:
         )
         g.set_facecolor('black')
         g.set_xticks(range(0, len(comm_df.index)), comm_df['Date'])
-        g.set_xticklabels(comm_df['Date'], rotation=90, fontsize=3, color='white')
+        g.set_xticklabels(comm_df['Date'], rotation=90, fontsize=3, color='gray')
         every_nth = 4
         for n, label in enumerate(g.xaxis.get_ticklabels()):
             if n % every_nth != 0:
@@ -70,8 +70,10 @@ for df_name in alldfs:
 
 
         g.set_yticks(g.get_yticks())
-        g.set_yticklabels(g.get_yticks(), size=5, color='white')
-        g.legend(loc='upper left', frameon=False, ncol=1, fontsize=5, labelcolor='white')
+        g_ylabels = ['{:,}'.format(y) for y in (g.get_yticks()).round(2)]
+        g.set_yticklabels(g_ylabels, size=5, color='gray')
+        g.set_ylim(0, max(g.get_yticks()))
+        g.legend(loc='upper left', frameon=False, ncol=1, fontsize=5, labelcolor='gray')
 
         ###############################
         #plt.tight_layout()
