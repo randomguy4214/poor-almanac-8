cb15569dbf78a9aa73f5f07ad10e18699b6ef971
randomguy <random@guy>
2022-05-25 10:06:04 +0200

    -

--- bundle_charts/plot_commodities.py
+++ bundle_charts/plot_commodities.py
@@ -24,30 +24,32 @@ commodities_df = pd.read_csv(os.path.join(cwd,input_folder,"2_processed_commodit
                              , index_col='Date')
 
 ### prepare sub dataframes
-crude_oil = commodities_df.iloc[:,[0,1,2,3]]
-coal = commodities_df.iloc[:,[4,5]]
-gas = commodities_df.iloc[:,[6,7,8]]
-gas_index = commodities_df.iloc[:,[9]]
-cocoa_coffee = commodities_df.iloc[:,[10,11,12, 47]]
-tea = commodities_df.iloc[:,[13,14,15,16]]
-oils = commodities_df.iloc[:,[17,21,22,24]]
-beans = commodities_df.iloc[:,[18,23,25,27]]
-plants = commodities_df.iloc[:,[19,20,26,28,29,30]]
-rice = commodities_df.iloc[:,[31,32,33,34]]
-wheat = commodities_df.iloc[:,[35,36]]
-fruits = commodities_df.iloc[:,[37,38,39]]
-meat = commodities_df.iloc[:,[40,41,42,43]]
-sugar = commodities_df.iloc[:,[44,45,46]]
-wood = commodities_df.iloc[:,[48,49,50,51,52]]
-materials_one = commodities_df.iloc[:,[53,54,55]]
-fertilizers = commodities_df.iloc[:,[56,57,58,59,60]]
-metals = commodities_df.iloc[:,[61,62,63,64,65,66,67,68,69,70]]
+a_crude_oil = commodities_df.iloc[:,[0,1,2,3]]
+b_coal = commodities_df.iloc[:,[4,5]]
+c_gas = commodities_df.iloc[:,[6,7,8]]
+cc_gas_index = commodities_df.iloc[:,[9]]
+d_cocoa_coffee = commodities_df.iloc[:,[10,11,12]]
+d_tobacco = commodities_df.iloc[:,[47]]
+d_tea = commodities_df.iloc[:,[13,14,15,16]]
+e_oils = commodities_df.iloc[:,[17,21,22,24]]
+e_beans = commodities_df.iloc[:,[18,23,25,27]]
+e_plants = commodities_df.iloc[:,[19,20,26,28,29,30]]
+e_rice = commodities_df.iloc[:,[31,32,33,34]]
+e_wheat = commodities_df.iloc[:,[35,36]]
+e_fruits = commodities_df.iloc[:,[37,38,39]]
+f_meat = commodities_df.iloc[:,[40,41,42,43]]
+e_sugar = commodities_df.iloc[:,[44,45,46]]
+f_wood = commodities_df.iloc[:,[48,49,50,51,52]]
+f_materials_one = commodities_df.iloc[:,[53,54,55]]
+e_fertilizers = commodities_df.iloc[:,[56,57,58,59,60]]
+g_metals_1 = commodities_df.iloc[:,[63,65,66,67]]
+g_metals_2 = commodities_df.iloc[:,[61,62,64,68,69,70]]
 
 # create a list of dataframes and loop through them to create separate charts
 alldfs = [var for var in dir() if isinstance(eval(var), pd.core.frame.DataFrame)]
 for df_name in alldfs:
     if df_name != 'commodities_df':
-        print(df_name)
+        print(df_name + ' plot')
         comm_df = eval(df_name) # searches for name of dataframe and activates it
         comm_df.reset_index(drop=False, inplace=True)
         #df_name_csv = df_name + '.csv'
